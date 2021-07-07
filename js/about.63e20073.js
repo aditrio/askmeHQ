e970ded07ec9eac99a3d7e43aa5dcb5ce8e26c30
aditrio <30noadi@gmail.com>
2021-07-07 13:39:18 +0700

    deploy

---
+++ js/about.63e20073.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,a,n){"use strict";n.r(a);var s=function(){var t=this,a=t.$createElement,n=t._self._c||a;return n("div",{staticClass:"about"},[n("v-row",{staticClass:"mt-4"},[n("v-col",{attrs:{align:"center"}},[n("h2",[t._v("TENTANG")])])],1)],1)},e=[],l=n("2877"),o=n("6544"),c=n.n(o),r=n("62ad"),u=n("0fd9"),i={},w=Object(l["a"])(i,s,e,!1,null,null,null);a["default"]=w.exports;c()(w,{VCol:r["a"],VRow:u["a"]})}}]);
+//# sourceMappingURL=about.63e20073.js.map
